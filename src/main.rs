e4a4a96005d6f9761c1ba1f618b4b473f94e3725
shunm-999 <shunmiyazaki7812@gmail.com>
2025-06-23 21:25:04 +0900

    add crap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+struct Args {
+    source: String,
+    target: String,
+}
+
 fn main() {
+    let args = Args::parse();
     println!("Hello, world!");
 }

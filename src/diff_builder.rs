acb166119e76d4f8785e9050b93d8a15413d5bd9
shunm-999 <shunmiyazaki7812@gmail.com>
2025-06-25 11:19:47 +0900

    refactoring

--- src/diff_builder.rs
+++ src/diff_builder.rs
@@ -3,23 +3,25 @@ use myers::myers::text_diff::{EditTag, TextDiff};
 
 pub(crate) struct DiffBuilder {}
 
+struct ChangeRange {
+    start: u64,
+    count: u64,
+}
+
 impl DiffBuilder {
     pub(crate) fn build(source_file: File, target_file: File) -> String {
         let mut result = String::new();
+
         let diff = TextDiff::from_lines(&source_file.content, &target_file.content);
+        let (old_change_range, new_change_range) = Self::calculate_change_range(&diff);
 
         Self::push_header(&mut result, &source_file, &target_file);
-        Self::push_change_range(&mut result, &diff);
+        Self::push_change_range(&mut result, &old_change_range, &new_change_range);
         Self::push_change(&mut result, &diff);
         result
     }
 
-    fn push_header(result: &mut String, source_file: &File, target_file: &File) {
-        result.push_str(&format!("--- {}\n", source_file.path));
-        result.push_str(&format!("+++ {}\n", target_file.path));
-    }
-
-    fn push_change_range(result: &mut String, diff: &TextDiff) {
+    fn calculate_change_range(diff: &TextDiff) -> (ChangeRange, ChangeRange) {
         let mut old_line_numbers = vec![];
         let mut new_line_numbers = vec![];
 
@@ -38,25 +40,42 @@ impl DiffBuilder {
             }
         }
 
-        let old_start = old_line_numbers.iter().min().copied().unwrap_or(0);
-        let old_end = old_line_numbers.iter().max().copied().unwrap_or(0);
-        let old_count = if old_line_numbers.is_empty() {
+        let old_change_range = Self::calculate_change_range_impl(&mut old_line_numbers);
+        let new_change_range = Self::calculate_change_range_impl(&mut new_line_numbers);
+
+        (old_change_range, new_change_range)
+    }
+
+    fn calculate_change_range_impl(line_numbers: &mut Vec<u64>) -> ChangeRange {
+        let old_start = line_numbers.iter().min().copied().unwrap_or(0);
+        let old_end = line_numbers.iter().max().copied().unwrap_or(0);
+        let old_count = if line_numbers.is_empty() {
             0
         } else {
             old_end - old_start + 1
         };
+        ChangeRange {
+            start: old_start,
+            count: old_count,
+        }
+    }
 
-        let new_start = new_line_numbers.iter().min().copied().unwrap_or(0);
-        let new_end = new_line_numbers.iter().max().copied().unwrap_or(0);
-        let new_count = if new_line_numbers.is_empty() {
-            0
-        } else {
-            new_end - new_start + 1
-        };
+    fn push_header(result: &mut String, source_file: &File, target_file: &File) {
+        result.push_str(&format!("--- {}\n", source_file.path));
+        result.push_str(&format!("+++ {}\n", target_file.path));
+    }
 
+    fn push_change_range(
+        result: &mut String,
+        old_change_range: &ChangeRange,
+        new_change_range: &ChangeRange,
+    ) {
         result.push_str(&format!(
             "@@ -{},{} +{},{} @@\n",
-            old_start, old_count, new_start, new_count
+            old_change_range.start,
+            old_change_range.count,
+            new_change_range.start,
+            new_change_range.count
         ));
     }
 

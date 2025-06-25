23c8b0e1f4cfb80f3c9d241060b3a9ed36c13c29
shunm-999 <shunmiyazaki7812@gmail.com>
2025-06-25 11:33:25 +0900

    Support for files without line breaks

--- src/diff_builder.rs
+++ src/diff_builder.rs
@@ -8,6 +8,18 @@ struct ChangeRange {
     count: u64,
 }
 
+impl ChangeRange {
+    fn start(&self) -> u64 {
+        self.start
+    }
+    fn end(&self) -> u64 {
+        self.start + self.count - 1
+    }
+    fn count(&self) -> u64 {
+        self.count
+    }
+}
+
 impl DiffBuilder {
     pub(crate) fn build(source_file: File, target_file: File) -> String {
         let mut result = String::new();
@@ -15,9 +27,19 @@ impl DiffBuilder {
         let diff = TextDiff::from_lines(&source_file.content, &target_file.content);
         let (old_change_range, new_change_range) = Self::calculate_change_range(&diff);
 
+        let old_has_new_line = &source_file.content.ends_with('\n');
+        let new_has_new_line = &target_file.content.ends_with('\n');
+
         Self::push_header(&mut result, &source_file, &target_file);
         Self::push_change_range(&mut result, &old_change_range, &new_change_range);
-        Self::push_change(&mut result, &diff);
+        Self::push_change(
+            &mut result,
+            &diff,
+            &old_change_range,
+            &new_change_range,
+            *old_has_new_line,
+            *new_has_new_line,
+        );
         result
     }
 
@@ -79,17 +101,39 @@ impl DiffBuilder {
         ));
     }
 
-    fn push_change(result: &mut String, diff: &TextDiff) {
+    fn push_change(
+        result: &mut String,
+        diff: &TextDiff,
+        old_change_range: &ChangeRange,
+        new_change_range: &ChangeRange,
+        old_has_new_line: bool,
+        new_has_new_line: bool,
+    ) {
         for edit in &diff.edits {
             match edit {
                 EditTag::Delete { old } => {
                     result.push_str(&format!("-{}\n", old.text));
+
+                    if !old_has_new_line && old.number == old_change_range.end() {
+                        result.push_str("\\ No newline at end of file\n");
+                    }
                 }
                 EditTag::Insert { new } => {
                     result.push_str(&format!("+{}\n", new.text));
+
+                    if !new_has_new_line && new.number == new_change_range.end() {
+                        result.push_str("\\ No newline at end of file\n");
+                    }
                 }
                 EditTag::Equal { old, new: _new } => {
                     result.push_str(&format!(" {}\n", old.text));
+
+                    if !old_has_new_line && old.number == old_change_range.end() {
+                        result.push_str("\\ No newline at end of file\n");
+                    }
+                    if !new_has_new_line && old.number == new_change_range.end() {
+                        result.push_str("\\ No newline at end of file\n");
+                    }
                 }
             }
         }
